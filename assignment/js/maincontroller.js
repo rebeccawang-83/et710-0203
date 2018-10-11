71aec06a82822d3bc0df77d96abbb39e4c3d8110
Student <jsitbon@qcc.cuny.edu>
2018-10-11 09:58:59 -0400

    codeacademy

---
+++ assignment/js/maincontroller.js
@@ -0,0 +1,8 @@
+app.controller('MainController', ['$scope', function($scope)
+ {
+  $scope.title = 'Top Sellers in Books';
+   $scope.promo = 'Queens borough';
+   $scope.product = {
+  	name: 'The Book of Trees',
+  	price: 19 
+}]);
