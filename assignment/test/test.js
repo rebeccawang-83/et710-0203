edd76524b5a2cde861f639de7782d35053d92717
Student <jsitbon@qcc.cuny.edu>
2018-10-16 15:33:21 -0400

    d

--- assignment/test/test.js
+++ assignment/test/test.js
@@ -5,3 +5,6 @@ describe('myApp', function(){
   it('In app.js, make sure to type var app = angular.module("myApp", []);', inject(function($controller) {
     expect(typeof $controller === 'function').toBe(true);
   }));
+
+
+});
