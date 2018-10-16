39cb607b7e8caa7ae332848b83862dac97edfee8
Student <jsitbon@qcc.cuny.edu>
2018-10-16 13:46:38 -0400

    '

--- assignment/js/maincontroller.js
+++
@@ -1,8 +0,0 @@
-app.controller('MainController', ['$scope', function($scope)
- {
-  $scope.title = 'Top Sellers in Books';
-   $scope.promo = 'Queens borough';
-   $scope.product = {
-  	name: 'The Book of Trees',
-  	price: 19 
-}]);
--- assignment/test/test.js
+++ assignment/test/test.js
@@ -2,11 +2,17 @@ describe('myApp', function(){
 
   beforeEach(module('myApp'));
 
-  it('Did you add $scope.product to the controller?', inject(function($controller) {
+  it('Did you create the function $scope.plusOne()?', inject(function($controller) {
     var scope = {},
           ctrl = $controller('MainController', {$scope:scope});
 
-    expect(scope.product).toEqual({name: "The Book of Trees", price: 19});
+    expect(scope.plusOne).toBeDefined();
   }));
 
+  it('On the <p class="likes"> element, make sure to use ng-click to call the plusOne() function.', inject(function($controller) {
+    var scope = {},
+          ctrl = $controller('MainController', {$scope:scope});
+    scope.plusOne(0);
+    expect(scope.products[0].likes).toBe(1);
+  }));
 });
